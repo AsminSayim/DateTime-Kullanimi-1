c27814accc450713df47ad13fd831b9e016b2d52
AsminSayim <145451871+AsminSayim@users.noreply.github.com>
2024-01-02 12:20:09 +0300

    Add files via upload

---
+++ DateTime-Kullanımı-1/Form1.Designer.cs
@@ -0,0 +1,353 @@
+﻿namespace DateTime_Kullanımı_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDate = new System.Windows.Forms.Button();
+            this.btnMonth = new System.Windows.Forms.Button();
+            this.btnDay = new System.Windows.Forms.Button();
+            this.btnYear = new System.Windows.Forms.Button();
+            this.btnDayOfWeek = new System.Windows.Forms.Button();
+            this.btnDayOfYear = new System.Windows.Forms.Button();
+            this.btnTimeOfDay = new System.Windows.Forms.Button();
+            this.btnHour = new System.Windows.Forms.Button();
+            this.btnMinute = new System.Windows.Forms.Button();
+            this.btnSecond = new System.Windows.Forms.Button();
+            this.btnMilliSecond = new System.Windows.Forms.Button();
+            this.btnTicks = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnDate
+            // 
+            this.btnDate.Location = new System.Drawing.Point(23, 32);
+            this.btnDate.Name = "btnDate";
+            this.btnDate.Size = new System.Drawing.Size(147, 31);
+            this.btnDate.TabIndex = 0;
+            this.btnDate.Text = "Date";
+            this.btnDate.UseVisualStyleBackColor = true;
+            this.btnDate.Click += new System.EventHandler(this.btnDate_Click);
+            // 
+            // btnMonth
+            // 
+            this.btnMonth.Location = new System.Drawing.Point(23, 69);
+            this.btnMonth.Name = "btnMonth";
+            this.btnMonth.Size = new System.Drawing.Size(147, 31);
+            this.btnMonth.TabIndex = 1;
+            this.btnMonth.Text = "Month";
+            this.btnMonth.UseVisualStyleBackColor = true;
+            this.btnMonth.Click += new System.EventHandler(this.btnMonth_Click);
+            // 
+            // btnDay
+            // 
+            this.btnDay.Location = new System.Drawing.Point(23, 106);
+            this.btnDay.Name = "btnDay";
+            this.btnDay.Size = new System.Drawing.Size(147, 31);
+            this.btnDay.TabIndex = 2;
+            this.btnDay.Text = "Day";
+            this.btnDay.UseVisualStyleBackColor = true;
+            this.btnDay.Click += new System.EventHandler(this.btnDay_Click);
+            // 
+            // btnYear
+            // 
+            this.btnYear.Location = new System.Drawing.Point(23, 143);
+            this.btnYear.Name = "btnYear";
+            this.btnYear.Size = new System.Drawing.Size(147, 31);
+            this.btnYear.TabIndex = 3;
+            this.btnYear.Text = "Year";
+            this.btnYear.UseVisualStyleBackColor = true;
+            this.btnYear.Click += new System.EventHandler(this.btnYear_Click);
+            // 
+            // btnDayOfWeek
+            // 
+            this.btnDayOfWeek.Location = new System.Drawing.Point(23, 180);
+            this.btnDayOfWeek.Name = "btnDayOfWeek";
+            this.btnDayOfWeek.Size = new System.Drawing.Size(147, 31);
+            this.btnDayOfWeek.TabIndex = 4;
+            this.btnDayOfWeek.Text = "DayOfWeek";
+            this.btnDayOfWeek.UseVisualStyleBackColor = true;
+            this.btnDayOfWeek.Click += new System.EventHandler(this.btnDayOfWeek_Click);
+            // 
+            // btnDayOfYear
+            // 
+            this.btnDayOfYear.Location = new System.Drawing.Point(23, 217);
+            this.btnDayOfYear.Name = "btnDayOfYear";
+            this.btnDayOfYear.Size = new System.Drawing.Size(147, 31);
+            this.btnDayOfYear.TabIndex = 5;
+            this.btnDayOfYear.Text = "DayOfYear";
+            this.btnDayOfYear.UseVisualStyleBackColor = true;
+            this.btnDayOfYear.Click += new System.EventHandler(this.btnDayOfYear_Click);
+            // 
+            // btnTimeOfDay
+            // 
+            this.btnTimeOfDay.Location = new System.Drawing.Point(23, 254);
+            this.btnTimeOfDay.Name = "btnTimeOfDay";
+            this.btnTimeOfDay.Size = new System.Drawing.Size(147, 31);
+            this.btnTimeOfDay.TabIndex = 6;
+            this.btnTimeOfDay.Text = "TimeOfDay";
+            this.btnTimeOfDay.UseVisualStyleBackColor = true;
+            this.btnTimeOfDay.Click += new System.EventHandler(this.btnTimeOfDay_Click);
+            // 
+            // btnHour
+            // 
+            this.btnHour.Location = new System.Drawing.Point(23, 291);
+            this.btnHour.Name = "btnHour";
+            this.btnHour.Size = new System.Drawing.Size(147, 31);
+            this.btnHour.TabIndex = 7;
+            this.btnHour.Text = "Hour";
+            this.btnHour.UseVisualStyleBackColor = true;
+            this.btnHour.Click += new System.EventHandler(this.btnHour_Click);
+            // 
+            // btnMinute
+            // 
+            this.btnMinute.Location = new System.Drawing.Point(23, 328);
+            this.btnMinute.Name = "btnMinute";
+            this.btnMinute.Size = new System.Drawing.Size(147, 31);
+            this.btnMinute.TabIndex = 8;
+            this.btnMinute.Text = "Minite";
+            this.btnMinute.UseVisualStyleBackColor = true;
+            this.btnMinute.Click += new System.EventHandler(this.btnMinute_Click);
+            // 
+            // btnSecond
+            // 
+            this.btnSecond.Location = new System.Drawing.Point(23, 365);
+            this.btnSecond.Name = "btnSecond";
+            this.btnSecond.Size = new System.Drawing.Size(147, 31);
+            this.btnSecond.TabIndex = 9;
+            this.btnSecond.Text = "Second";
+            this.btnSecond.UseVisualStyleBackColor = true;
+            this.btnSecond.Click += new System.EventHandler(this.btnSecond_Click);
+            // 
+            // btnMilliSecond
+            // 
+            this.btnMilliSecond.Location = new System.Drawing.Point(23, 402);
+            this.btnMilliSecond.Name = "btnMilliSecond";
+            this.btnMilliSecond.Size = new System.Drawing.Size(147, 31);
+            this.btnMilliSecond.TabIndex = 10;
+            this.btnMilliSecond.Text = "MilliSecond";
+            this.btnMilliSecond.UseVisualStyleBackColor = true;
+            this.btnMilliSecond.Click += new System.EventHandler(this.btnMilliSecond_Click);
+            // 
+            // btnTicks
+            // 
+            this.btnTicks.Location = new System.Drawing.Point(23, 439);
+            this.btnTicks.Name = "btnTicks";
+            this.btnTicks.Size = new System.Drawing.Size(147, 31);
+            this.btnTicks.TabIndex = 11;
+            this.btnTicks.Text = "Ticks";
+            this.btnTicks.UseVisualStyleBackColor = true;
+            this.btnTicks.Click += new System.EventHandler(this.btnTicks_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(206, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 24);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(206, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 24);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(206, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 24);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(206, 146);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 24);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(206, 183);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 24);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(206, 220);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(66, 24);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(206, 257);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(66, 24);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(206, 294);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(66, 24);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "label8";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(206, 331);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(66, 24);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "label9";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(206, 368);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(77, 24);
+            this.label10.TabIndex = 21;
+            this.label10.Text = "label10";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(206, 405);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(77, 24);
+            this.label11.TabIndex = 22;
+            this.label11.Text = "label11";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(206, 438);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(77, 24);
+            this.label12.TabIndex = 23;
+            this.label12.Text = "label12";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(548, 523);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnTicks);
+            this.Controls.Add(this.btnMilliSecond);
+            this.Controls.Add(this.btnSecond);
+            this.Controls.Add(this.btnMinute);
+            this.Controls.Add(this.btnHour);
+            this.Controls.Add(this.btnTimeOfDay);
+            this.Controls.Add(this.btnDayOfYear);
+            this.Controls.Add(this.btnDayOfWeek);
+            this.Controls.Add(this.btnYear);
+            this.Controls.Add(this.btnDay);
+            this.Controls.Add(this.btnMonth);
+            this.Controls.Add(this.btnDate);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDate;
+        private System.Windows.Forms.Button btnMonth;
+        private System.Windows.Forms.Button btnDay;
+        private System.Windows.Forms.Button btnYear;
+        private System.Windows.Forms.Button btnDayOfWeek;
+        private System.Windows.Forms.Button btnDayOfYear;
+        private System.Windows.Forms.Button btnTimeOfDay;
+        private System.Windows.Forms.Button btnHour;
+        private System.Windows.Forms.Button btnMinute;
+        private System.Windows.Forms.Button btnSecond;
+        private System.Windows.Forms.Button btnMilliSecond;
+        private System.Windows.Forms.Button btnTicks;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+    }
+}
+
---
+++ DateTime-Kullanımı-1/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DateTime_Kullanımı_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DateTime bugun = DateTime.Now;
+        private void btnDate_Click(object sender, EventArgs e)
+        {
+            label1.Text = bugun.Date.ToString();
+        }
+
+        private void btnMonth_Click(object sender, EventArgs e)
+        {
+            label2.Text = bugun.Month.ToString();
+        }
+
+        private void btnDay_Click(object sender, EventArgs e)
+        {
+            label3.Text = bugun.Day.ToString();
+        }
+
+        private void btnYear_Click(object sender, EventArgs e)
+        {
+            label4.Text = bugun.Year.ToString();
+        }
+
+        private void btnDayOfWeek_Click(object sender, EventArgs e)
+        {
+            label5.Text = bugun.DayOfWeek.ToString();
+        }
+
+        private void btnDayOfYear_Click(object sender, EventArgs e)
+        {
+            label6.Text = bugun.DayOfYear.ToString();
+        }
+
+        private void btnTimeOfDay_Click(object sender, EventArgs e)
+        {
+            label7.Text = bugun.TimeOfDay.ToString();
+        }
+
+        private void btnHour_Click(object sender, EventArgs e)
+        {
+            label8.Text = bugun.Hour.ToString();
+        }
+
+        private void btnMinute_Click(object sender, EventArgs e)
+        {
+            label9.Text = bugun.Minute.ToString();
+
+        }
+
+        private void btnSecond_Click(object sender, EventArgs e)
+        {
+            label10.Text = bugun.Second.ToString();
+        }
+
+        private void btnMilliSecond_Click(object sender, EventArgs e)
+        {
+            label11.Text = bugun.Millisecond.ToString();
+        }
+
+        private void btnTicks_Click(object sender, EventArgs e)
+        {
+            label12.Text=bugun.Ticks.ToString();
+        }
+    }
+}
